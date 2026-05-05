d9b444f3e830b8413002674120d0f37d1da07f38
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-05-05 09:44:47 +0600

    refactor: replace enum with const assertions for UserRoles

--- src/2.9_Use_as_const_instead_of_Enum.ts
+++ src/2.9_Use_as_const_instead_of_Enum.ts
@@ -1,18 +1,39 @@
 // Use as const instead of Enum (recommended)
 // as const assertion
 
-enum UserRoles2 {
-	Admin = "admin",
-	Editor = "editor",
-	Viewer = "viewer",
-}
+// enum UserRoles {
+// 	Admin = "Admin",
+// 	Editor = "Editor",
+// 	Viewer = "Viewer",
+// }
 
-const canEdit2 = (role: UserRoles2) => {
+const UserRoles = {
+	Admin: "Admin",
+	Editor: "Editor",
+	Viewer: "Viewer",
+} as const;
+
+const canEdit = (role: keyof typeof UserRoles) => {
+	if (role === UserRoles.Admin || role === UserRoles.Editor) {
+		return true;
+	} else return false;
+};
+
+const isEditPermissible = canEdit(UserRoles.Admin);
+console.log(isEditPermissible);
+
+//
+const UserRoles2 = {
+	Admin: "ADMIN",
+	Editor: "EDITOR",
+	Viewer: "VIEWER",
+} as const;
+
+const canEdit2 = (role: (typeof UserRoles2)[keyof typeof UserRoles2]) => {
 	if (role === UserRoles2.Admin || role === UserRoles2.Editor) {
 		return true;
 	} else return false;
 };
 
 const isEditPermissible2 = canEdit2(UserRoles2.Admin);
-
 console.log(isEditPermissible2);
